332f0dd44d49b86baef69c1ff0d8b9ef47d0289a
Enes <enesbaskale@gmail.com>
2023-01-27 14:02:42 +0300

    DevTools Aktif etme

---
+++ src/main/java/utils/ConfigProgramming.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class ConfigProgramming {
+
+    //DevTools ==> LiveReload
+    //file ==> settings ==> compiler ==> build project automaticlly (OK clicked)(ctrl + alt + s)
+//    file ==> Advanced settings ==> allow auto-make to start even (OKclicked)
+
+}
