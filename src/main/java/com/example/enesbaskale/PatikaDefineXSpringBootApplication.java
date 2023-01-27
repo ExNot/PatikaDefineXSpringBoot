e789e8301c75fee535821d4125de28189070e8b0
Enes <enesbaskale@gmail.com>
2023-01-27 12:40:15 +0300

    first commit (patika)

---
+++ src/main/java/com/example/enesbaskale/PatikaDefineXSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.enesbaskale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaDefineXSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaDefineXSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enesbaskale/PatikaDefineXSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enesbaskale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaDefineXSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
