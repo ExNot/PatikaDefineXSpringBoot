ebde0d5ecc41aecdd567973742935e3d798f9b80
Enes <enesbaskale@gmail.com>
2023-01-27 14:02:13 +0300

    SpringBootApplication ==> Security(exclude)

--- src/main/java/com/example/enesbaskale/PatikaDefineXSpringBootApplication.java
+++ src/main/java/com/example/enesbaskale/PatikaDefineXSpringBootApplication.java
@@ -3,7 +3,12 @@ package com.example.enesbaskale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Security inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+
 public class PatikaDefineXSpringBootApplication {
 
     public static void main(String[] args) {
