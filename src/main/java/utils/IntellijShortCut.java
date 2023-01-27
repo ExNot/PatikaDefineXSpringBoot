3ef0120a136a0881de5d04e8445ff08ae071fb4b
Enes <enesbaskale@gmail.com>
2023-01-27 14:02:28 +0300

    kısayollar

---
+++ src/main/java/utils/IntellijShortCut.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class IntellijShortCut {
+
+
+//    Setting ==> ctrl+alt+s
+//    Alt+Insert ==> Generate
+//    ctrl+/ ==> single comment
+//    ctrl+shift+/ ==> single comment
+
+
+
+}
+
