7d72cfcaf314eac045b7791401894a183d17ff68
Enes <enesbaskale@gmail.com>
2023-01-31 23:36:22 +0300

    @Repository (JPA Hibernate)
    JpaRepository>CrudRepository
    extends JpaRepository
    extends CrudRepository

---
+++ src/main/java/com/example/enesbaskale/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.enesbaskale.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//JpaRepository>CrudRepository
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+
+
+
+}
